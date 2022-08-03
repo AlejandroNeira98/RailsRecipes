a16ee761985400397a98aac95e035e8cad4df880
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-03 10:14:08 -0500

    Generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
+  get 'general_shopping_list/index'
+  get 'general_shopping_list/new'
+  get 'general_shopping_list/create'
+  get 'recipes/index'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/destroy'
+  get 'recipes/show'
+  get 'foods/index'
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
