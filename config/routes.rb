1aa37e0f6a12c7579557eef108ced8a89e37c33d
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-03 10:25:09 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
-  get 'public_recipes/index'
-  get 'general_shopping_list/index'
+  get 'public_recipes/', to: 'public_recipes#index'
+  get 'general_shopping_list/', to: 'general_shopping_list#index'
   get 'general_shopping_list/new'
-  get 'general_shopping_list/create'
-  get 'recipes/index'
+  post 'general_shopping_list/create', to: 'general_shopping_list#create'
+  get 'recipes/', to: 'recipes#index'
   get 'recipes/new'
-  get 'recipes/create'
-  get 'recipes/destroy'
-  get 'recipes/show'
-  get 'foods/index'
+  post 'recipes/create', to: 'recipes#create'
+  delete 'recipes/destroy', to: 'recipes#destroy'
+  get 'recipes/:id', to: 'recipes#show'
+  get 'foods/', to: 'foods#index'
   get 'foods/new'
-  get 'foods/create'
-  get 'foods/destroy'
+  post 'foods/create', to: 'foods#create'
+  delete 'foods/destroy', to: 'foods#destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  root to: 'devise/sessions#new'
 end
