d09f03dc903cf4202ed8e66ba459fec28f1758b4
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-08-04 10:17:17 -0500

    Update foods destroy action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,7 +4,7 @@ class FoodsController < ApplicationController
   def index
     if current_user
       @foods = current_user.foods
-    else 
+    else
       redirect_to new_user_session_path
     end
   end
@@ -16,5 +16,8 @@ class FoodsController < ApplicationController
     redirect_to foods_path if food.create
   end
 
-  def destroy; end
+  def destroy
+    food = Food.find(params[:food_id])
+    redirect_to foods_path if food.destroy
+  end
 end
