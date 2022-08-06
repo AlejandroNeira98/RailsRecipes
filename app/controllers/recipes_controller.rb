c6ae8236bdc88563607d49f551da7474ad7df623
Chris Clothier <c.r.clothier@gmail.com>
2022-08-06 11:47:59 +0100

    Create recipes list view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    if current_user
+      @recipes = current_user.recipes
+    else
+      redirect_to new_user_session_path
+    end
+  end
 
   def new; end
 
-  def create; end
+  def create
+    recipe = Recipe.new(name: params[:name], preparation_time: params[:preparation_time], cooking_time: params[:cooking_time], description: params[:description], public: true, user_id: current_user.id)
+    redirect_to recipes_path if recipe.save
+  end
 
   def destroy; end
 
